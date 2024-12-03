c1d0e48fe5c0529ccc6eadf144112f34be5ac5ce
Lucifer <luciferxhnh@gmail.com>
2024-12-03 23:01:54 +0700

    fix dist

--- dist/api/v1/routes/index.routes.js
+++ dist/api/v1/routes/index.routes.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const city_routes_1 = require("./city.routes");
 const routesVersion1 = (app) => {
-    const version = "/api/v1/duongits";
-    app.use(version + "/", city_routes_1.cityRoutes);
+    app.use(city_routes_1.cityRoutes);
 };
 exports.default = routesVersion1;
